41b05e8e51d3db35f4066516829a1b578e9c5434
Tiernan OToole <tiernan@tiernanotoole.ie>
2018-07-25 20:07:57 +0100

    starting work on #18

---
+++ BlackVueDownloader.PCL/DownloadOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackVueDownloader.PCL
+{
+	public class DownloadOptions
+	{
+		[Option('i', "ip", Required = true, HelpText = "IP Address of BlackVue camera")]
+		public string IPAddr { get; set; }
+
+		[Option('d', "directory", Required = true, HelpText = "The Local directory you want files written to")]
+		public string OutputDirectory { get; set; }
+
+		[Option('l', "last", Required = false, HelpText = "Download Last X days of files")]
+		public int? LastDays { get; set; }
+	}
+}
